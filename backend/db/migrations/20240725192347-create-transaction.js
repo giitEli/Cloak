828c314c3b8ba49c4c312d57d0853a3d89b4f99f
Eli <eli.suffridge@gmail.com>
2024-07-25 12:29:49 -0700

    transactions model and migration file

--- backend/db/migrations/20240725192347-create-transaction.js
+++ backend/db/migrations/20240725192347-create-transaction.js
@@ -1,40 +1,51 @@
-'use strict';
-/** @type {import('sequelize-cli').Migration} */
+"use strict";
+
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
+
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Transactions', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
+    await queryInterface.createTable(
+      "Transactions",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        type: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        symbol: {
+          type: Sequelize.STRING,
+        },
+        amount: {
+          type: Sequelize.STRING,
+        },
+        price: {
+          type: Sequelize.STRING,
+        },
+        total: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
       },
-      type: {
-        type: Sequelize.STRING
-      },
-      symbol: {
-        type: Sequelize.STRING
-      },
-      amount: {
-        type: Sequelize.STRING
-      },
-      price: {
-        type: Sequelize.STRING
-      },
-      total: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
+      options
+    );
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Transactions');
-  }
+    await queryInterface.dropTable("Transactions");
+  },
 };
